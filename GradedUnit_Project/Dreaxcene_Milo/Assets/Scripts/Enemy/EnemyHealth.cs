a27dead72185374063464107beaa21e8d9637756
Smudge04 <nathanmpsmith2004@gmail.com>
2023-05-08 23:09:29 +0100

    more enemies, basic health bar
    Health bar needs to have rotation sorted out but wasnt working so left a comment on the youtube video

--- GradedUnit_Project/Dreaxcene_Milo/Assets/Scripts/Enemy/EnemyHealth.cs
+++ GradedUnit_Project/Dreaxcene_Milo/Assets/Scripts/Enemy/EnemyHealth.cs
@@ -3,23 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class EnemyHealth : MonoBehaviour
+public class EnemyHealth : MonoBehaviour //NS
 {
     [SerializeField] private float health, maxHealth; //Variable to store enemy health, allows for each enemy to have a different health
 
-    private void FixedUpdate() //NS
-    {
+    [SerializeField] FloatingHealthBar healthBar;
 
-        if (health <= 0) //If enemy dies
-        {
-            Destroy(gameObject); //Destroy this object
-        }
+    private void Start()
+    {
+        health = maxHealth;
+        healthBar.UpdateHealthBar(health, maxHealth);
+    }
+
+    private void Awake()
+    {
+        healthBar = GetComponentInChildren<FloatingHealthBar>();
     }
 
     public void TakeDamage(int Damage)
     {
         health -= Damage; //Take damage equal to bullet damage
+        healthBar.UpdateHealthBar(health, maxHealth);
         Debug.Log(health); //shows enemy health on debug log
+
+        if (health <= 0) //If enemy dies
+        {
+            Destroy(gameObject); //Destroy this object
+        }
     }
 
 
