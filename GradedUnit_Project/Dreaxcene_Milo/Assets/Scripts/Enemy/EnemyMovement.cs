369dd7bc118f053453390409b1c9227bac034f7c
Smudge04 <nathanmpsmith2004@gmail.com>
2023-05-18 20:06:47 +0100

    nothing really
    If someone could add enemy collision with each other that would be great

--- GradedUnit_Project/Dreaxcene_Milo/Assets/Scripts/Enemy/EnemyMovement.cs
+++ GradedUnit_Project/Dreaxcene_Milo/Assets/Scripts/Enemy/EnemyMovement.cs
@@ -10,24 +10,23 @@ public class EnemyMovement : MonoBehaviour
     private Transform playerPos; //variable that stores where the player is
 
     Vector2 PlayerDir; //Variable that the direction the player is in for the enemy to follow
-    private Rigidbody2D rb; //A vriable that stores enemy RigidBody when enemy is spawned
 
     [SerializeField]
     private float repelRange; //Stores the distance an enemy can get before they can no longer move forward
 
     [SerializeField] ParticleSystem EnemyParticle;
 
-
     private void Awake()
     {
-        playerPos = GameObject.FindGameObjectWithTag("Player").transform; //Finds object with tag player and its location and stores it within playerPos
-        rb = GetComponent<Rigidbody2D>();  //Gets enemy rigidbody       
+        playerPos = GameObject.FindGameObjectWithTag("Player").transform; //Finds object with tag player and its location and stores it within playerPos 
     }
+
 
     private void Update()
     {
         if (Vector2.Distance(transform.position, playerPos.position) > repelRange) //If enemy is further than repel distance, enemy will move towards the player
             transform.position = Vector2.MoveTowards(transform.position, playerPos.position, speed * Time.deltaTime);
+ 
 
         Vector2 targetPos = playerPos.transform.position; //Enemy will target the position of the player
        
